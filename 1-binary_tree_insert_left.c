6ca8006a1ee845317c94fe33fe25f388485f722c
Yaeluribe <2962@holbertonschool.com>
2021-11-30 15:44:30 -0500

    corrected

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -27,6 +27,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		parent->left = new_node;
 		new_node->parent = parent;
+		new_node->left = NULL;
+		new_node->right = NULL;
 	}
 	return (new_node);
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -27,6 +27,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	{
 		parent->right = new_node;
 		new_node->parent = parent;
+		new_node->left = NULL;
+		new_node->right = NULL;
 	}
 	return (new_node);
 }
